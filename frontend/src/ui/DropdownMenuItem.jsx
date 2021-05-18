64b3c4668b38ced7491af617967d5122e94d7481
pajecawav <pajecawav@gmail.com>
2021-05-18 21:29:40 +0300

    make tabs header responsive

--- frontend/src/ui/DropdownMenuItem.jsx
+++ frontend/src/ui/DropdownMenuItem.jsx
@@ -1,7 +1,12 @@
-export function DropdownMenuItem({ icon: Icon, text, ...props }) {
+import clsx from "clsx";
+
+export function DropdownMenuItem({ className, icon: Icon, text, ...props }) {
     return (
         <div
-            className="flex items-center gap-1 px-2 py-1 cursor-pointer transition-colors duration-100 hover:bg-primary-600"
+            className={clsx(
+                "flex items-center gap-1 px-2 py-1 cursor-pointer transition-colors duration-100 hover:bg-primary-600",
+                className
+            )}
             {...props}
         >
             {Icon && <Icon className="h-5" />}
